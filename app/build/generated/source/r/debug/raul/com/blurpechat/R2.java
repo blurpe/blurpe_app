8a8f63a87a0e2f1b13c9cd01525c4cec75c678e4
blurpe <blurpgroup@gmail.com>
2017-02-13 21:17:16 +0200

    Add files via upload

---
+++ app/build/generated/source/r/debug/raul/com/blurpechat/R2.java
@@ -0,0 +1,446 @@
+// Generated code from Butter Knife gradle plugin. Do not modify!
+package raul.com.blurpechat;
+
+import android.support.annotation.AttrRes;
+import android.support.annotation.ColorRes;
+import android.support.annotation.DimenRes;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.IdRes;
+import android.support.annotation.IntegerRes;
+import android.support.annotation.StringRes;
+
+public final class R2 {
+  public static final class attr {
+    @AttrRes
+    public static final int buttonSize = 0x7f01000b;
+
+    @AttrRes
+    public static final int circleCrop = 0x7f010006;
+
+    @AttrRes
+    public static final int civ_border_color = 0x7f010001;
+
+    @AttrRes
+    public static final int civ_border_overlay = 0x7f010002;
+
+    @AttrRes
+    public static final int civ_border_width = 0x7f010000;
+
+    @AttrRes
+    public static final int civ_fill_color = 0x7f010003;
+
+    @AttrRes
+    public static final int colorScheme = 0x7f01000c;
+
+    @AttrRes
+    public static final int imageAspectRatio = 0x7f010005;
+
+    @AttrRes
+    public static final int imageAspectRatioAdjust = 0x7f010004;
+
+    @AttrRes
+    public static final int layoutManager = 0x7f010007;
+
+    @AttrRes
+    public static final int reverseLayout = 0x7f010009;
+
+    @AttrRes
+    public static final int scopeUris = 0x7f01000d;
+
+    @AttrRes
+    public static final int spanCount = 0x7f010008;
+
+    @AttrRes
+    public static final int stackFromEnd = 0x7f01000a;
+  }
+
+  public static final class color {
+    @ColorRes
+    public static final int accent = 0x7f080000;
+
+    @ColorRes
+    public static final int bg_user_message = 0x7f080001;
+
+    @ColorRes
+    public static final int common_google_signin_btn_text_dark = 0x7f080012;
+
+    @ColorRes
+    public static final int common_google_signin_btn_text_dark_default = 0x7f080002;
+
+    @ColorRes
+    public static final int common_google_signin_btn_text_dark_disabled = 0x7f080003;
+
+    @ColorRes
+    public static final int common_google_signin_btn_text_dark_focused = 0x7f080004;
+
+    @ColorRes
+    public static final int common_google_signin_btn_text_dark_pressed = 0x7f080005;
+
+    @ColorRes
+    public static final int common_google_signin_btn_text_light = 0x7f080013;
+
+    @ColorRes
+    public static final int common_google_signin_btn_text_light_default = 0x7f080006;
+
+    @ColorRes
+    public static final int common_google_signin_btn_text_light_disabled = 0x7f080007;
+
+    @ColorRes
+    public static final int common_google_signin_btn_text_light_focused = 0x7f080008;
+
+    @ColorRes
+    public static final int common_google_signin_btn_text_light_pressed = 0x7f080009;
+
+    @ColorRes
+    public static final int divider = 0x7f08000a;
+
+    @ColorRes
+    public static final int foreground_material_light = 0x7f08000b;
+
+    @ColorRes
+    public static final int primary = 0x7f08000c;
+
+    @ColorRes
+    public static final int primary_dark = 0x7f08000d;
+
+    @ColorRes
+    public static final int primary_text_default_material_light = 0x7f08000e;
+
+    @ColorRes
+    public static final int text_color = 0x7f08000f;
+
+    @ColorRes
+    public static final int text_color_icon = 0x7f080010;
+
+    @ColorRes
+    public static final int username = 0x7f080011;
+  }
+
+  public static final class dimen {
+    @DimenRes
+    public static final int activity_horizontal_margin = 0x7f070000;
+
+    @DimenRes
+    public static final int activity_vertical_margin = 0x7f070001;
+
+    @DimenRes
+    public static final int connection_status_padding = 0x7f070002;
+
+    @DimenRes
+    public static final int item_touch_helper_max_drag_scroll_per_frame = 0x7f070003;
+
+    @DimenRes
+    public static final int item_touch_helper_swipe_escape_max_velocity = 0x7f070004;
+
+    @DimenRes
+    public static final int item_touch_helper_swipe_escape_velocity = 0x7f070005;
+
+    @DimenRes
+    public static final int log_in_margin = 0x7f070006;
+  }
+
+  public static final class drawable {
+    @DrawableRes
+    public static final int attach = 0x7f020000;
+
+    @DrawableRes
+    public static final int common_full_open_on_phone = 0x7f020001;
+
+    @DrawableRes
+    public static final int common_google_signin_btn_icon_dark = 0x7f020002;
+
+    @DrawableRes
+    public static final int common_google_signin_btn_icon_dark_disabled = 0x7f020003;
+
+    @DrawableRes
+    public static final int common_google_signin_btn_icon_dark_focused = 0x7f020004;
+
+    @DrawableRes
+    public static final int common_google_signin_btn_icon_dark_normal = 0x7f020005;
+
+    @DrawableRes
+    public static final int common_google_signin_btn_icon_dark_pressed = 0x7f020006;
+
+    @DrawableRes
+    public static final int common_google_signin_btn_icon_light = 0x7f020007;
+
+    @DrawableRes
+    public static final int common_google_signin_btn_icon_light_disabled = 0x7f020008;
+
+    @DrawableRes
+    public static final int common_google_signin_btn_icon_light_focused = 0x7f020009;
+
+    @DrawableRes
+    public static final int common_google_signin_btn_icon_light_normal = 0x7f02000a;
+
+    @DrawableRes
+    public static final int common_google_signin_btn_icon_light_pressed = 0x7f02000b;
+
+    @DrawableRes
+    public static final int common_google_signin_btn_text_dark = 0x7f02000c;
+
+    @DrawableRes
+    public static final int common_google_signin_btn_text_dark_disabled = 0x7f02000d;
+
+    @DrawableRes
+    public static final int common_google_signin_btn_text_dark_focused = 0x7f02000e;
+
+    @DrawableRes
+    public static final int common_google_signin_btn_text_dark_normal = 0x7f02000f;
+
+    @DrawableRes
+    public static final int common_google_signin_btn_text_dark_pressed = 0x7f020010;
+
+    @DrawableRes
+    public static final int common_google_signin_btn_text_light = 0x7f020011;
+
+    @DrawableRes
+    public static final int common_google_signin_btn_text_light_disabled = 0x7f020012;
+
+    @DrawableRes
+    public static final int common_google_signin_btn_text_light_focused = 0x7f020013;
+
+    @DrawableRes
+    public static final int common_google_signin_btn_text_light_normal = 0x7f020014;
+
+    @DrawableRes
+    public static final int common_google_signin_btn_text_light_pressed = 0x7f020015;
+
+    @DrawableRes
+    public static final int headshot_7 = 0x7f020016;
+
+    @DrawableRes
+    public static final int ic_launcher = 0x7f020017;
+
+    @DrawableRes
+    public static final int ic_logo = 0x7f020018;
+
+    @DrawableRes
+    public static final int ic_send_white_24dp = 0x7f020019;
+
+    @DrawableRes
+    public static final int recipient_rounded_corners = 0x7f02001a;
+
+    @DrawableRes
+    public static final int send_button_corners = 0x7f02001b;
+
+    @DrawableRes
+    public static final int sender_rounded_corners = 0x7f02001c;
+  }
+
+  public static final class id {
+    @IdRes
+    public static final int action_logout = 0x7f090022;
+
+    @IdRes
+    public static final int action_settings = 0x7f090021;
+
+    @IdRes
+    public static final int adjust_height = 0x7f090001;
+
+    @IdRes
+    public static final int adjust_width = 0x7f090002;
+
+    @IdRes
+    public static final int auto = 0x7f090007;
+
+    @IdRes
+    public static final int btn_cancel_register = 0x7f09001b;
+
+    @IdRes
+    public static final int btn_login = 0x7f090013;
+
+    @IdRes
+    public static final int btn_register = 0x7f090014;
+
+    @IdRes
+    public static final int btn_register_user = 0x7f09001a;
+
+    @IdRes
+    public static final int btn_send_message = 0x7f09000e;
+
+    @IdRes
+    public static final int dark = 0x7f090008;
+
+    @IdRes
+    public static final int divider = 0x7f09000b;
+
+    @IdRes
+    public static final int edit_text_display_name = 0x7f090017;
+
+    @IdRes
+    public static final int edit_text_email_login = 0x7f090011;
+
+    @IdRes
+    public static final int edit_text_email_register = 0x7f090018;
+
+    @IdRes
+    public static final int edit_text_message = 0x7f09000d;
+
+    @IdRes
+    public static final int edit_text_password_log_in = 0x7f090012;
+
+    @IdRes
+    public static final int edit_text_password_register = 0x7f090019;
+
+    @IdRes
+    public static final int icon_only = 0x7f090004;
+
+    @IdRes
+    public static final int img_avatar = 0x7f09001e;
+
+    @IdRes
+    public static final int item_touch_helper_previous_elevation = 0x7f090000;
+
+    @IdRes
+    public static final int light = 0x7f090009;
+
+    @IdRes
+    public static final int none = 0x7f090003;
+
+    @IdRes
+    public static final int progress_bar_users = 0x7f090016;
+
+    @IdRes
+    public static final int progress_for_chat = 0x7f09000f;
+
+    @IdRes
+    public static final int recycler_view_chat = 0x7f09000a;
+
+    @IdRes
+    public static final int recycler_view_users = 0x7f090015;
+
+    @IdRes
+    public static final int senderContainer = 0x7f09000c;
+
+    @IdRes
+    public static final int standard = 0x7f090005;
+
+    @IdRes
+    public static final int text_view_connection_status = 0x7f09001f;
+
+    @IdRes
+    public static final int text_view_display_name = 0x7f090020;
+
+    @IdRes
+    public static final int text_view_recipient_message = 0x7f09001c;
+
+    @IdRes
+    public static final int text_view_sender_message = 0x7f09001d;
+
+    @IdRes
+    public static final int titleChat = 0x7f090010;
+
+    @IdRes
+    public static final int wide = 0x7f090006;
+  }
+
+  public static final class integer {
+    @IntegerRes
+    public static final int google_play_services_version = 0x7f0a0000;
+  }
+
+  public static final class string {
+    @StringRes
+    public static final int action_settings = 0x7f050011;
+
+    @StringRes
+    public static final int app_name = 0x7f050012;
+
+    @StringRes
+    public static final int common_google_play_services_enable_button = 0x7f050000;
+
+    @StringRes
+    public static final int common_google_play_services_enable_text = 0x7f050001;
+
+    @StringRes
+    public static final int common_google_play_services_enable_title = 0x7f050002;
+
+    @StringRes
+    public static final int common_google_play_services_install_button = 0x7f050003;
+
+    @StringRes
+    public static final int common_google_play_services_install_text = 0x7f050004;
+
+    @StringRes
+    public static final int common_google_play_services_install_title = 0x7f050005;
+
+    @StringRes
+    public static final int common_google_play_services_notification_ticker = 0x7f050006;
+
+    @StringRes
+    public static final int common_google_play_services_unknown_issue = 0x7f050007;
+
+    @StringRes
+    public static final int common_google_play_services_unsupported_text = 0x7f050008;
+
+    @StringRes
+    public static final int common_google_play_services_update_button = 0x7f050009;
+
+    @StringRes
+    public static final int common_google_play_services_update_text = 0x7f05000a;
+
+    @StringRes
+    public static final int common_google_play_services_update_title = 0x7f05000b;
+
+    @StringRes
+    public static final int common_google_play_services_updating_text = 0x7f05000c;
+
+    @StringRes
+    public static final int common_google_play_services_wear_update_text = 0x7f05000d;
+
+    @StringRes
+    public static final int common_open_on_phone = 0x7f05000e;
+
+    @StringRes
+    public static final int common_signin_button_text = 0x7f05000f;
+
+    @StringRes
+    public static final int common_signin_button_text_long = 0x7f050010;
+
+    @StringRes
+    public static final int default_web_client_id = 0x7f050013;
+
+    @StringRes
+    public static final int error_fields_empty = 0x7f050014;
+
+    @StringRes
+    public static final int error_incorrect_email_pass = 0x7f050015;
+
+    @StringRes
+    public static final int firebase_database_url = 0x7f050016;
+
+    @StringRes
+    public static final int gcm_defaultSenderId = 0x7f050017;
+
+    @StringRes
+    public static final int google_api_key = 0x7f050018;
+
+    @StringRes
+    public static final int google_app_id = 0x7f050019;
+
+    @StringRes
+    public static final int google_crash_reporting_api_key = 0x7f05001a;
+
+    @StringRes
+    public static final int google_storage_bucket = 0x7f05001b;
+
+    @StringRes
+    public static final int login_error_message = 0x7f05001c;
+
+    @StringRes
+    public static final int login_error_title = 0x7f05001d;
+
+    @StringRes
+    public static final int name = 0x7f05001e;
+
+    @StringRes
+    public static final int title_activity_chat = 0x7f05001f;
+
+    @StringRes
+    public static final int title_activity_log_in = 0x7f050020;
+
+    @StringRes
+    public static final int title_activity_register = 0x7f050021;
+  }
+}
